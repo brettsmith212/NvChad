759a08fbc484a2db62f4d29456dabeddd5f67790
brettsmith212 <brettsmith212@gmail.com>
2024-02-13 15:56:41 -0800

    title setting

--- init.lua
+++ init.lua
@@ -19,3 +19,28 @@ end
 dofile(vim.g.base46_cache .. "defaults")
 vim.opt.rtp:prepend(lazypath)
 require "plugins"
+
+-- Enable setting the window title
+vim.o.title = true
+
+-- Use Lua functions to replicate the Vimscript expressions
+local function set_titlestring()
+  local hostname = vim.loop.os_gethostname()
+  local filepath = vim.fn.expand('%:p') -- gets the full path of the file in the buffer
+  local filename = vim.fn.expand('%:t') -- gets the tail of the file path (just the file name)
+  local last_modified = os.date('%Y-%m-%d %H:%M', vim.fn.getftime(vim.fn.expand('%')))
+
+  -- Set the titlestring using the information gathered above
+  -- You can modify the string to format it as you like
+  vim.o.titlestring = filepath .. ' [last modified: ' .. last_modified .. ']'
+end
+
+-- Call the function to set the titlestring
+set_titlestring()
+
+-- Optionally, you can also set an autocommand to update the titlestring whenever you switch buffers or write to a file
+vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter", "BufWritePost"}, {
+  pattern = "*",
+  callback = set_titlestring
+})
+
