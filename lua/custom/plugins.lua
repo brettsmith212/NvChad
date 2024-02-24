d523595596a6d01f6522f35e42e564a052ed033e
brettsmith212 <brettsmith212@gmail.com>
2024-02-23 19:37:31 -0800

    debug w/ arguments working

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,83 +1,114 @@
 local plugins = {
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "clangd",
-        "clang-format",
-        "codelldb",
-        "black@24.1.1",
-        "debugpy@1.8.0",
-        "mypy",
-        "ruff@0.2.0",
-        "pyright",
-        "lua-language-server"
-      }
+    {
+        "williamboman/mason.nvim",
+        opts = {
+            ensure_installed = {
+                "clangd",
+                "clang-format",
+                "codelldb",
+                "black@24.1.1",
+                "debugpy@1.8.0",
+                "mypy",
+                "ruff@0.2.0",
+                "pyright",
+                "lua-language-server",
+                "codelldb"
+            }
+        }
+    },
+    {
+        "rcarriga/nvim-dap-ui",
+        event = "VeryLazy",
+        dependencies = "mfussenegger/nvim-dap",
+        config = function()
+            local dap = require("dap")
+            local dapui = require("dapui")
+            dapui.setup()
+            dap.listeners.after.event_initialized["dapui_config"] = function()
+                dapui.open()
+            end
+            dap.listeners.before.event_terminated["dapui_config"] = function()
+                dapui.close()
+            end
+            dap.listeners.before.event_exited["dapui_config"] = function()
+                dapui.close()
+            end
+        end
+    },
+    {
+        "jay-babu/mason-nvim-dap.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "mfussenegger/nvim-dap"
+        },
+        opts = {
+            handlers = {}
+        },
+    },
+    {
+        "mfussenegger/nvim-dap",
+        config = function()
+            local dap = require('dap')
+            dap.adapters.lldb = {
+                type = 'server',
+                port = "${port}",
+                executable = {
+                    command = '/Users/brettsmith/.local/share/nvim/mason/bin/codelldb',
+                    args = { "--port", "${port}" },
+                }
+            }
+
+
+            dap.configurations.cpp = {
+                {
+                    name = "Debug with Arguments",
+                    type = "codelldb",
+                    request = "launch",
+                    program = function()
+                        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+                    end,
+                    -- args = { "-l", "hello" },
+                    args = function()
+                        local args_string = vim.fn.input('Arguments: ')
+                        return vim.split(args_string, " +")
+                    end,
+                    cwd = '${workspaceFolder}',
+                    stopOnEntry = false,
+                },
+            }
+
+            require("core.utils").load_mappings("dap")
+        end
+    },
+    {
+        "mfussenegger/nvim-dap-python",
+        ft = "python",
+        dependencies = {
+            "mfussenegger/nvim-dap",
+            "rcarriga/nvim-dap-ui",
+        },
+        config = function(_, opts)
+            local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+            require("dap-python").setup(path)
+            require("core.utils").load_mappings("dap_python")
+        end,
+    },
+    {
+        "jose-elias-alvarez/null-ls.nvim",
+        ft = { "python" },
+        event = "VeryLazy",
+        opts = function()
+            return require "custom.configs.null-ls"
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require "plugins.configs.lspconfig"
+            require "custom.configs.lspconfig"
+        end
     }
-  },
-  {
-    "rcarriga/nvim-dap-ui",
-    event = "VeryLazy",
-    dependencies = "mfussenegger/nvim-dap",
-    config = function()
-      local dap = require("dap")
-      local dapui = require("dapui")
-      dapui.setup()
-      dap.listeners.after.event_initialized["dapui_config"] = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated["dapui_config"] = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited["dapui_config"] = function()
-        dapui.close()
-      end
-    end
-  },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "mfussenegger/nvim-dap"
-    },
-    opts = {
-      handlers = {}
-    },
-  },
-  {
-    "mfussenegger/nvim-dap",
-    config = function(_, _)
-      require("core.utils").load_mappings("dap")
-    end
-  },
-  {
-    "mfussenegger/nvim-dap-python",
-    ft = "python",
-    dependencies = {
-      "mfussenegger/nvim-dap",
-      "rcarriga/nvim-dap-ui",
-    },
-    config = function(_, opts)
-      local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
-      require("dap-python").setup(path)
-      require("core.utils").load_mappings("dap_python")
-    end,
-  },
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    ft = {"python"},
-    event = "VeryLazy",
-    opts = function()
-      return require "custom.configs.null-ls"
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end
-  }
 }
+
 return plugins
