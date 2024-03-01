a2129eae69485ef8b61d27e913875fc10ce868ff
brettsmith212 <brettsmith212@gmail.com>
2024-02-29 21:38:15 -0800

    go support

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -1,5 +1,5 @@
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-local null_ls = require("null-ls")
+local null_ls = require "null-ls"
 
 local opts = {
   sources = {
@@ -7,18 +7,22 @@ local opts = {
     null_ls.builtins.formatting.black,
     null_ls.builtins.diagnostics.mypy,
     null_ls.builtins.diagnostics.ruff,
+    null_ls.builtins.formatting.gofumpt,           -- go install github.com/incu6us/goimports-reviser/v3@latest
+    null_ls.builtins.formatting.goimports_reviser, -- go install mvdan.cc/gofumpt@latest
+    null_ls.builtins.formatting.golines,           -- go install github.com/segmentio/golines@latest
+    null_ls.builtins.formatting.stylua,
   },
-  on_attach= function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({
+  on_attach = function(client, bufnr)
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds {
         group = augroup,
         buffer = bufnr,
-      })
+      }
       vim.api.nvim_create_autocmd("BufWritePre", {
         group = augroup,
         buffer = bufnr,
         callback = function()
-          vim.lsp.buf.format({ bufnr = bufnr })
+          vim.lsp.buf.format { bufnr = bufnr }
         end,
       })
     end
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,114 +1,135 @@
 local plugins = {
-    {
-        "williamboman/mason.nvim",
-        opts = {
-            ensure_installed = {
-                "clangd",
-                "clang-format",
-                "codelldb",
-                "black@24.1.1",
-                "debugpy@1.8.0",
-                "mypy",
-                "ruff@0.2.0",
-                "pyright",
-                "lua-language-server",
-                "codelldb"
-            }
-        }
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "clangd",
+        "clang-format",
+        "codelldb",
+        "black@24.1.1",
+        "debugpy@1.8.0",
+        "mypy",
+        "ruff@0.2.0",
+        "pyright",
+        "lua-language-server",
+        "codelldb",
+        "gopls",
+        "stylua",
+      },
     },
-    {
-        "rcarriga/nvim-dap-ui",
-        event = "VeryLazy",
-        dependencies = "mfussenegger/nvim-dap",
-        config = function()
-            local dap = require("dap")
-            local dapui = require("dapui")
-            dapui.setup()
-            dap.listeners.after.event_initialized["dapui_config"] = function()
-                dapui.open()
-            end
-            dap.listeners.before.event_terminated["dapui_config"] = function()
-                dapui.close()
-            end
-            dap.listeners.before.event_exited["dapui_config"] = function()
-                dapui.close()
-            end
-        end
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    event = "VeryLazy",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require "dap"
+      local dapui = require "dapui"
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap",
     },
-    {
-        "jay-babu/mason-nvim-dap.nvim",
-        event = "VeryLazy",
-        dependencies = {
-            "williamboman/mason.nvim",
-            "mfussenegger/nvim-dap"
+    opts = {
+      handlers = {},
+    },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require "dap"
+      dap.adapters.lldb = {
+        type = "server",
+        port = "${port}",
+        executable = {
+          command = "/Users/brettsmith/.local/share/nvim/mason/bin/codelldb",
+          args = { "--port", "${port}" },
         },
-        opts = {
-            handlers = {}
-        },
-    },
-    {
-        "mfussenegger/nvim-dap",
-        config = function()
-            local dap = require('dap')
-            dap.adapters.lldb = {
-                type = 'server',
-                port = "${port}",
-                executable = {
-                    command = '/Users/brettsmith/.local/share/nvim/mason/bin/codelldb',
-                    args = { "--port", "${port}" },
-                }
-            }
+      }
 
-            dap.configurations.cpp = {
-                {
-                    name = "Debug with Arguments",
-                    type = "codelldb",
-                    request = "launch",
-                    -- program = '/path/to/task/file',
-                    program = function()
-                        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-                    end,
-                    -- args = { "-l", "flags" },
-                    args = function()
-                        local args_string = vim.fn.input('Arguments: ')
-                        return vim.split(args_string, " +")
-                    end,
-                    cwd = '${workspaceFolder}',
-                    stopOnEntry = false,
-                },
-            }
-
-            require("core.utils").load_mappings("dap")
-        end
-    },
-    {
-        "mfussenegger/nvim-dap-python",
-        ft = "python",
-        dependencies = {
-            "mfussenegger/nvim-dap",
-            "rcarriga/nvim-dap-ui",
+      dap.configurations.cpp = {
+        {
+          name = "Debug with Arguments",
+          type = "codelldb",
+          request = "launch",
+          -- program = '/path/to/task/file',
+          program = function()
+            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+          end,
+          -- args = { "-l", "flags" },
+          args = function()
+            local args_string = vim.fn.input "Arguments: "
+            return vim.split(args_string, " +")
+          end,
+          cwd = "${workspaceFolder}",
+          stopOnEntry = false,
         },
-        config = function(_, opts)
-            local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
-            require("dap-python").setup(path)
-            require("core.utils").load_mappings("dap_python")
-        end,
+      }
+
+      require("core.utils").load_mappings "dap"
+    end,
+  },
+  {
+    "mfussenegger/nvim-dap-python",
+    ft = "python",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "rcarriga/nvim-dap-ui",
     },
-    {
-        "jose-elias-alvarez/null-ls.nvim",
-        ft = { "python" },
-        event = "VeryLazy",
-        opts = function()
-            return require "custom.configs.null-ls"
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            require "plugins.configs.lspconfig"
-            require "custom.configs.lspconfig"
-        end
-    }
+    config = function(_, opts)
+      local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+      require("dap-python").setup(path)
+      require("core.utils").load_mappings "dap_python"
+    end,
+  },
+  {
+    "leoluz/nvim-dap-go",
+    ft = "go",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function(_, opts)
+      require("dap-go").setup(opts)
+      require("core.utils").load_mappings "dap_go"
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = { "python", "go", "lua" },
+    event = "VeryLazy",
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    config = function(_, opts)
+      require("gopher").setup(opts)
+    end,
+    build = function()
+      vim.cmd [[silent! GoInstallDeps]]
+    end,
+  },
 }
 
 return plugins
